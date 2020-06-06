6bfed89c490d2c9d928c4d1187af137de43ad114
JuliaWieczorek <wieczorek.julia.76@gmail.com>
2020-06-06 16:11:50 +0200

    create class Client and CNN

--- FetchMail.py
+++ FetchMail.py
@@ -1,10 +1,7 @@
 import email
 import imaplib
-import os
 import tkinter as tk
 from PIL import Image, ImageTk
-#from time import time
-#from imageai.Detection import ObjectDetection
 import os
 
 class MainApplication(tk.Frame):
@@ -13,16 +10,12 @@ class MainApplication(tk.Frame):
         tk.Frame.__init__(self, root)
         root.title("Name")
         bg_image = tk.PhotoImage(file="background-869596_1280.png")
-        #background = tk.Label(image=bg_image)
-        #background.image = bg_image
-        #background.pack()
+        # background = tk.Label(image=bg_image)
+        # background.image = bg_image
+        # background.pack()
         self.scrollFrame = ScrollFrame(self)
         path = 'C:/Users/julia/Documents/bioinformatyka/magisterka/images/'
-        #number = len(os.listdir(path))
-        #frame = tk.Frame(root).pack()
-        #canvas = tk.Canvas(frame, bg='cornsilk4', width=n * number, height=n * number)
-        #canvas.pack(expand='yes', fill='both')
-        #canvas.create_image(50, 10, image=bg_image, anchor='nw')
+
         i = 0
         for name in os.listdir(path):
             # image = tk.PhotoImage(path + "/" + name)
@@ -33,38 +26,19 @@ class MainApplication(tk.Frame):
             label.image = ph
             label.pack()
 
-
-            #c = tk.Canvas(canvas, width=n, height=n)
-            #c.create_image(n / 2, n / 2, image=ph)
-            #canvas.create_window(0, n * i, window=c)
-            #i += 1
         self.scrollFrame.pack(side='top', fill='both', expand=True)
-        '''
-        vsb = tk.Scrollbar(frame, orient="vertical")
-        vsb.pack(side='right', fill='y')
-        vsb.config(command=canvas.yview)
-        canvas.config(width=n * number, height=n * number)
-        canvas.config(yscrollcommand=vsb.set, scrollregion=canvas.bbox('all'))
-        canvas.pack(side='left', expand=True, fill='both')
-        self.parent = parent
-    
-        # Gets the requested values of the height and widht.
-        windowWidth = root.winfo_reqwidth()
-        windowHeight = root.winfo_reqheight()
-        positionRight = int(root.winfo_screenwidth()/8 - windowWidth/8)
-        positionDown = int(root.winfo_screenheight()/8 - windowHeight/8)
-        # Positions the window in the center of the page.
-        root.geometry("1000x800+{}+{}".format(positionRight, positionDown)) # jeszcze jest za nisko :/
-    '''
+
+
+        # TODO: połaczyć z Clientem!!!!!
         self.logowanie()
 
     def printMsg(self, msg):
         print(msg)
 
     def logowanie(self):
-        #self.screen = tk.Tk()
+        # self.screen = tk.Tk()
         self.screen = tk.Toplevel()
-        self.screen.title("Log in") #panel logowania
+        self.screen.title("Log in")  # panel logowania
         # Gets the requested values of the height and widht.
         windowWidth = root.winfo_reqwidth()
         windowHeight = root.winfo_reqheight()
@@ -77,61 +51,91 @@ class MainApplication(tk.Frame):
         self.screen.grid_columnconfigure(0, weight=1)
         self.screen.grid_rowconfigure(0, weight=1)
         tk.Label(self.screen, text=' ', bg='dim gray').grid(column=0, row=1)
+
         tk.Label(self.screen, text="email", bg='dim gray').grid(column=0, row=2)
         self.email = tk.Entry(self.screen, width=20)
         self.email.grid(column=1, row=2)
+
         tk.Label(self.screen, text='password', bg='dim gray').grid(column=0, row=3)
         self.password = tk.Entry(self.screen, width=20)
         self.password.grid(column=1, row=3)
+
+        # self.dane = [self.email.cget("text"), self.password.cget("text")]
+
+        # self.client = Client(self.dane[0], self.dane[1])
+        # self.client = Client(self.get_label(self.email), self.get_label(self.password))
+        # self.client = Client('mgrphototrap@onet.pl', 'Mgr.Photo.Trap.1')
+        self.client = Client(self.email.cget("text"), self.password.cget("text"))
+
         tk.Label(self.screen, text=' ', bg='dim gray').grid(column=4, row=4)
         tk.Button(self.screen, text="Log in", command=self.login, bg='AntiqueWhite2').grid(columnspan=4, row=5)
-        self.message = tk.Label(self.screen,  bg='dim gray')
-        self.message.grid(columnspan=4 , row=7)
-        #self.screen.call('wm', 'attributes', '.', '-topmost', '1')
+        # tk.Button(self.screen, text="Log in", command=self.client.printuj, bg='AntiqueWhite2').grid(columnspan=4, row=5)
+        # tk.Button(self.screen, text="Log in", command=self.printuj, bg='AntiqueWhite2').grid(columnspan=4, row=5)
+
+        # self.client.message = tk.Label(self.screen, bg='dim gray')
+        # self.client.message.grid(columnspan=4, row=7)
+        self.message = tk.Label(self.screen, bg='dim gray')
+        self.message.grid(columnspan=4, row=7)
+
+        # self.screen.call('wm', 'attributes', '.', '-topmost', '1')
 
     def login(self):
         try:
             imap = imaplib.IMAP4_SSL("imap.poczta.onet.pl", 993)
+            print(self.email.get(), self.password.get(), 'login try in Main')
             imap.login(self.email.get(), self.password.get())
             print("Login accepted")
             self.message['text'] = 'Login accepted'
             self.screen.after(1000, self.screen.destroy)
         except:
+            print(self.email.get(), self.password.get(), 'login except in Main')
             print("An exception occurred")
-            self.message['text'] = 'Incorrect email or password' #invalid?
+            self.message['text'] = 'Incorrect email or password'  # invalid?
+
+    # def get_label(self, variable):
+    #    self.variable = variable
+    #    return self.variable.get()
+
+    # def printuj(self):
+    #    print(self.get_label(self.email), self.get_label(self.password))
 
 class ScrollFrame(tk.Frame):
     def __init__(self, parent):
-        super().__init__(parent) # create a frame (self)
+        super().__init__(parent)  # create a frame (self)
 
-        self.canvas = tk.Canvas(self, borderwidth=0, background="#ffffff")          #place canvas on self
-        self.viewPort = tk.Frame(self.canvas, background="#ffffff")   #place a frame on the canvas, this frame will hold the child widgets
-        self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview) #place a scrollbar on self
-        self.canvas.configure(yscrollcommand=self.vsb.set)           #attach scrollbar action to scroll of canvas
+        self.canvas = tk.Canvas(self, borderwidth=0, background="#ffffff")
+        self.viewPort = tk.Frame(self.canvas, background="#ffffff")
+        self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
 
-        self.vsb.pack(side="right", fill="y")                                       #pack scrollbar to right of self
-        self.canvas.pack(side="left", fill="both", expand=True)          #pack canvas to left of self and expand to fil
-        self.canvas_window = self.canvas.create_window((4,4), window=self.viewPort, anchor="nw", tags="self.viewPort")
-        #add view port frame to canvas
+        self.vsb.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas_window = self.canvas.create_window((4, 4), window=self.viewPort, anchor="nw", tags="self.viewPort")
+        # add view port frame to canvas
 
+        self.viewPort.bind("<Configure>",
+                           self.onFrameConfigure)  # bind an event whenever the size of the viewPort frame changes.
+        self.canvas.bind("<Configure>",
+                         self.onCanvasConfigure)  # bind an event whenever the size of the viewPort frame changes.
 
-        self.viewPort.bind("<Configure>", self.onFrameConfigure) #bind an event whenever the size of the viewPort frame changes.
-        self.canvas.bind("<Configure>", self.onCanvasConfigure)  #bind an event whenever the size of the viewPort frame changes.
-
-        self.onFrameConfigure(None) #perform an initial stretch on render, otherwise the scroll region has a tiny border until the first resize
+        self.onFrameConfigure(
+            None)  # perform an initial stretch on render, otherwise the scroll region has a tiny border until the first resize
 
     def onFrameConfigure(self, event):
         '''Reset the scroll region to encompass the inner frame'''
-        self.canvas.configure(scrollregion=self.canvas.bbox("all"))                 #whenever the size of the frame changes, alter the scroll region respectively.
+        self.canvas.configure(scrollregion=self.canvas.bbox(
+            "all"))  # whenever the size of the frame changes, alter the scroll region respectively.
 
     def onCanvasConfigure(self, event):
         '''Reset the canvas window to encompass inner frame when required'''
         canvas_width = event.width
-        self.canvas.itemconfig(self.canvas_window, width = canvas_width)            #whenever the size of the canvas changes alter the window region respectively.
+        self.canvas.itemconfig(self.canvas_window,
+                               width=canvas_width)  # whenever the size of the canvas changes alter the window region respectively.
 
+class FetchEmail(object):
 
+    # TODO: połaczyć z CNN
 
-class FetchEmail():
     def unread(username, password):
         imap = imaplib.IMAP4_SSL("imap.poczta.onet.pl", 993)
         imap.login(username, password)
@@ -147,7 +151,6 @@ class FetchEmail():
         for num in data[0].split():
             typ, data = imap.fetch(num, '(RFC822)')
         imap.logout()
-
 
     def read(username, password):
         imap = imaplib.IMAP4_SSL("imap.poczta.onet.pl", 993)
@@ -173,7 +176,7 @@ class FetchEmail():
                         fp = open(filePath, 'wb')
                         fp.write(part.get_payload(decode=True))
                         fp.close()
-                        #subject = str(email_message).split("Subject: ", 1)[1].split("\nTo:", 1)[0]
+                        # subject = str(email_message).split("Subject: ", 1)[1].split("\nTo:", 1)[0]
                     #  print('Downloaded "{file}" from email titled "{subject}" with UID {uid}.'.format(file=fileName, subject=subject,uid=latest_email_uid.decode('utf-8')))
             for response_part in data:
                 if isinstance(response_part, tuple):
@@ -183,22 +186,91 @@ class FetchEmail():
                     print('From : ' + email_from + '\n')
                     print('Subject : ' + email_subject + '\n')
                     print(msg.get_payload(decode=True))
-'''
-class DetectionImage():
-    def detection(self):
-        execution_path = os.getcwd()
 
-        detector = ObjectDetection()
-        detector.setModelTypeAsRetinaNet()
-        detector.setModelPath(os.path.join(execution_path, "resnet50_coco_best_v2.0.1.h5"))
-        detector.loadModel()
-        detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path, "image.jpg"),
-                                                     output_image_path=os.path.join(execution_path, "imagenew.jpg"))
+class Client(object):
 
-        for eachObject in detections:
-            print(eachObject["name"], " : ", eachObject["percentage_probability"])
+    # TODO: połączyć z MainApp powinno być self.client(self.main.get(), self.main.password())
 
-'''
+    ID_default = 1
+    list_of_clients = []
+
+    def __init__(self, mail, password):
+        self.client_ID = Client.ID_default
+        Client.ID_default = Client.ID_default + 1
+        self.mail = mail
+        self.password = password
+        self.logged = 0  # zalogowany
+        self.__class__.list_of_clients.append(self)
+
+    def log_in(self):
+
+        if (self.logged == 0):
+            try:
+                imap = imaplib.IMAP4_SSL("imap.poczta.onet.pl", 993)
+                imap.login(self.mail, self.password)
+                print(self.mail, self.password, 'try')
+                print("Login accepted")
+                self.message['text'] = 'Login accepted'
+                self.logged = 1
+                self.screen.after(1000, self.screen.destroy)
+            except:
+                print(self.mail, self.password, 'except')
+                print("An exception occurred")
+                self.message['text'] = 'Incorrect email or password'  # invalid?
+
+        else:
+            print("You are already logged in")
+            self.message['text'] = "You are already logged in"  # invalid?
+
+    def printuj(self):
+        print(type(self.mail), self.password)  ## nie
+
+class CNN(object):
+    # TODO: sprawdzić czy działa
+
+    def __init__(self):
+        import keras
+        import tensorflow as tf
+
+        try:
+            self.sess = tf.compat.v1.Session()
+            self.model = keras.models.load_model('convnet_cifar10.kerasave')
+            self.classifier()
+        except:
+            exec(open('settings.py').read())
+            exec(open("functions.py").read())
+            exec(open("sample_images.py").read())
+            exec(open("model.py").read())
+            exec(open("model_analysis.py").read())
+            exec(open("init_training.py").read())
+            exec(open("long_training.py").read())
+            exec(open("short_training.py").read())
+            self.classifier()
+
+    def classifier(self):
+        # TODO: zmienić plik - ma być ten, który pobierany zostaje z maila
+        # TODO: sprawdzic czy działa
+
+        exec(open("functions.py").read())
+        import numpy as np
+
+        _, _, _, _, labels = setup_load_cifar()
+
+        self.img = tf.io.read_file(
+            'C:/Users/julia/Documents/bioinformatyka/classification_images/cats_and_dogs_filtered/cat.1.jpg')
+        self.img = tf.image.decode_jpeg(self.img, channels=3)
+        self.img.set_shape([None, None, 3])
+        self.img = tf.image.resize(self.img, (32, 32))
+        from keras.preprocessing import image
+        self.img = image.img_to_array(self.img)  # convert to numpy array
+        self.img = np.expand_dims(self.img, 0)  # make 'batch' of 1
+
+        self.pred = self.model.predict(self.img)
+        self.pred = labels["label_names"][np.argmax(self.pred)]
+        print(self.pred)
+
+
+
 server = 'imap.poczta.onet.pl'
 username = 'mgrphototrap@onet.pl'
 password = 'Mgr.Photo.Trap.1'
